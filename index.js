a9904393d8694dfdae88fd666bd1643820bec40c
kumarsahitya <ksahitya27@gmail.com>
2025-02-09 20:04:28 +0530

    restructure directories and files

--- index.js
+++ index.js
@@ -2,15 +2,12 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 dotenv.config();
-const connectDB = require('./db');
-const admin= require('./routes/admin')
+require('./loader/db');
+const index= require('./routes/index')
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-
-connectDB();
 
 app.use(bodyParser.json());
 
@@ -18,7 +15,7 @@ app.get("/", (req, res) => {
   res.json({ message: "API Working" });
 });
 
-app.use("/admin", admin);
+app.use('/', index)
 
 
 
---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+
+const admin = require('./admin/admin');
+router.use('/admin', admin);
+
+module.exports = router;
