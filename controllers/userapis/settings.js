66493c4108f7c8ee98d83b857ba6a27127697a57
kumarsahitya <ksahitya27@gmail.com>
2025-03-15 14:07:43 +0530

    setting response format change

--- controllers/userapis/settings.js
+++ controllers/userapis/settings.js
@@ -2,20 +2,20 @@ const yogaworkoutSetting = require('../../models/settings');
 
 const settings = async (req, res) => {
 	try {
-		let settings = await yogaworkoutSetting.find().sort({ createdAt: -1 });
+		let settings = await yogaworkoutSetting.findOne().sort({ createdAt: -1 });
 		if (settings.length === 0) {
 			return res.status(400).json({
-				data: { success: 0, settings: [], error: 'Please Try Again' },
+				data: { success: 0, setting: [], error: 'Please Try Again' },
 			});
 		} else {
 			res.status(200).json({
-				data: { success: 1, settings: settings, error: '' },
+				data: { success: 1, setting: settings, error: '' },
 			});
 		}
 	} catch (e) {
 		console.error(e);
 		res.status(500).json({
-			data: { success: 0, settings: [], error: 'Server Error' },
+			data: { success: 0, setting: [], error: 'Server Error' },
 		});
 	}
 };
