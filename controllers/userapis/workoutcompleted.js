35363aef640e32da2f1383ec0553434a8f938e08
kapilsahitya <kapilsahitya96@gmail.com>
2025-03-17 00:01:09 +0530

    workoutcompleted

--- controllers/userapis/workoutcompleted.js
+++ controllers/userapis/workoutcompleted.js
@@ -12,28 +12,44 @@ const workoutCompleted = async (req, res) => {
 		) {
 			const checkuserLogin = await checkUserLogin(data.user_id, data.session, data.device_id);
 			if (checkuserLogin) {
-				if (data.week_id && data.week_id != "") {
-					const newWeekCompleted = new yogaworkoutWeekCompleted({
-						user_id: data.user_id,
-						week_id: data.week_id,
-						is_completed: 1
+				if (data.workout_type && data.workout_type != "" &&
+					data.workout_id && data.workout_id != "" &&
+					data.kcal && data.kcal != "" &&
+					data.completed_duration && data.completed_duration != "" &&
+					data.completed_date && data.completed_date != ""
+				) {
+					const date_years = data.completed_date.split("-")
+					const date_year = date_years[1] + "-" + date_years[0];
+					const newWorkoutCompleted = new yogaworkoutworkoutCompleted({
+						workout_type: data.workout_type,
+						workout_id: data.workout_id,
+						kcal: data.kcal,
+						completed_duration: data.completed_duration,
+						completed_date: data.completed_date,
+						date_year: date_year
 					})
 
-					await newWeekCompleted.save();
-					res.status(201).json({ data: { success: 1, weekcompleted: newWeekCompleted, error: 'Week Completed Successfully!' } });
+					const addedWorkoutCompleted = await newWorkoutCompleted.save();
+					if (addedWorkoutCompleted) {
+						res.status(201).json({ data: { success: 1, workoutscompleted: addedWorkoutCompleted, error: 'Add Workout Completed!' } });
+					}
+					else{
+						res.status(201).json({ data: { success: 0, workoutscompleted: [], error: 'Please Try Again!' } });
+					}
+
 				}
 				else {
-					res.status(201).json({ data: { success: 0, weekcompleted: [], error: 'Variable not set!' } });
+					res.status(201).json({ data: { success: 0, workoutscompleted: [], error: 'Variable not set!' } });
 				}
 			}
 			else {
-				res.status(201).json({ data: { success: 0, weekcompleted: [], error: 'Please login first!' } });
+				res.status(201).json({ data: { success: 0, workoutscompleted: [], error: 'Please login first!' } });
 			}
 		}
 	} catch (e) {
 		console.error(e);
 		res.status(500).json({
-			data: { success: 0, week: [], error: 'Server Error' },
+			data: { success: 0, workoutscompleted: [], error: 'Server Error' },
 		});
 	}
 };
